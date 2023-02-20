623f590963c536931f647bd2c7252b91211adfba
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2023-02-20 17:16:14 -0500

    finished second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, numTimes) {
+    let output = '';
+    if(numTimes < 0){
+        return "ERROR";
+    }
+    for(i=0;i<numTimes;i++){
+        output = output.concat(string);
+    }
+    return output;
 };
+
+repeatString('testing', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
