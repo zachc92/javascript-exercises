ccbbccc0484c1057715bb6c4fb462a7be0434589
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2023-02-22 09:30:02 -0500

    finished fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, arg) {
+    for(argCounter=1;argCounter<arguments.length;argCounter++){
+        for(i=0;i<array.length;i++){
+            if(arguments[argCounter] === array[i]){
+                array.splice(i,1);
+            }
+        }
+    }
+    return array;
 };
+
+removeFromArray([1,2,3,4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
