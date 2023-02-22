9f4249aa5db42642bdaaa785f86546a5c4993192
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2023-02-21 19:43:37 -0500

    finished third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringHolder = string.split("");
+    let stringOutput = [];
+    let stringCount = stringHolder.length;
+    for(i=stringCount; i>=0; i--){
+        stringOutput.push(stringHolder[i]);
+    }
+    return stringOutput.join('');
 };
+
+reverseString('hello there');
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
--- 03_reverseString/solution/reverseString-solution.js
+++ 03_reverseString/solution/reverseString-solution.js
@@ -1,5 +1,11 @@
-const reverseString = function (string) {
-  return string.split("").reverse().join("");
+const reverseString = function(string) {
+    let stringHolder = string.split("");
+    let stringOutput = [];
+    let stringCount = stringHolder.length;
+    for(i=stringCount; i>=0; i--){
+        stringOutput.push(stringHolder[i]);
+    }
+    return stringOutput.join('');
 };
 
 module.exports = reverseString;
