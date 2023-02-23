cc31817de21dba47f9d26692811d1b3614974278
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2023-02-22 20:22:07 -0500

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(num1 < 0 || num2 < 0 || typeof(num1) !== 'number' || typeof(num2) !== 'number'){
+        return "ERROR";
+    }
+    if(num1 < num2){
+        for(i=num1;i<=num2;i++){
+            sum += i;
+        }
+    } else if (num1 > num2){
+        for(i=num2;i<=num1;i++){
+            sum += i;
+        }
+    }
+    return sum;
 };
+
+sumAll(1, 10);
 
 // Do not edit below this line
 module.exports = sumAll;
