8a957a1c07ecdc9d77e1072cc864e44955499a96
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2023-02-26 15:51:10 -0500

    finished eleventh exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let output = [];
+    for(i=0;i<books.length;i++){
+        output.push(books[i].title);
+    }
+    return output;
 };
 
 // Do not edit below this line
