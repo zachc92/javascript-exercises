be270d4d7848eda2d2dc874c3b76fc0ecfb05b40
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2023-02-26 16:04:56 -0500

    finished twelfth exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = {};
+    let oldestAge = 0;
+    for(i=0;i<people.length;i++){
+        if(people[i].yearOfDeath == undefined){
+            people[i].yearOfDeath = 2023;
+        }
+        if(oldest == ""){
+            oldest = people[i];
+            oldestAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        } else if(people[i].yearOfDeath - people[i].yearOfBirth > oldestAge){
+            oldest = people[i];
+            oldestAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
