9fc281dd8675883c1bc1d4aef9cced47861656bd
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2023-02-26 15:45:39 -0500

    finished tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let output = 0;
+    let operator = 1;
+    let holder = 1;
+    if(Number(num) == 1 || Number(num) == 2){
+        return 1;
+    } else if(Number(num) < 0) {
+        return "OOPS";
+    }
+    for(i=1;i<Number(num)-1;i++){
+        output = operator + holder;
+        holder = operator;
+        operator = output;
+    }
+    return output;
 };
 
 // Do not edit below this line
