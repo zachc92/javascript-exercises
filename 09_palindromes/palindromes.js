bdd1465cf5b2a090717503db11c98fd9cb83b0da
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2023-02-26 15:26:06 -0500

    finished ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let splitWord = word.toLowerCase();
+    splitWord = splitWord.split('');
+    let palindromeTest = [];
+    let palindrome = [];
+    for(i=0;i<splitWord.length;i++){
+        if(splitWord[i] == "!" || splitWord[i] == "," || splitWord[i] == "." || splitWord[i] == " "){
+            continue;
+        }
+        palindrome.unshift(splitWord[i]);
+    }
+    for(i=0;i<splitWord.length;i++){
+        if(splitWord[i] == "!" || splitWord[i] == "," || splitWord[i] == "." || splitWord[i] == " "){
+            continue;
+        }
+        palindromeTest.push(splitWord[i]);
+    }
+    console.log(palindrome.join(''));
+    console.log(palindromeTest.join(''));
+    if(palindrome.join('') == palindromeTest.join('')){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
