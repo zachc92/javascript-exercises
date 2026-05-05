4e6efa359c7066ecceb947018e288eae706aeff1
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2026-05-04 21:18:22 -0400

    Complete javascript exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = [];
+    let array = string.split('');
+    for(i = 0; i < array.length; i++){
+        newString.unshift(array[i]);
+    }
+    return newString.join('');
 };
 
 // Do not edit below this line
