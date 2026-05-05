aa26ef02a77da40e9a7dffe5dd18073efa94f227
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2026-05-04 22:23:32 -0400

    Complete javascript exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0 || 
+    Number.isInteger(a) == false || Number.isInteger(b) == false){
+        return 'ERROR';
+    }
+    if(a > b){
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    let sum = 0;
+    for(i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum;
 };
+
+sumAll(2.5,4)
 
 // Do not edit below this line
 module.exports = sumAll;
