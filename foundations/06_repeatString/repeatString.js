eeeca61755e39e59db360c8d0e76cb8f8d4b5901
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2026-05-04 21:12:13 -0400

    Complete javascript exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0){
+        return 'ERROR';
+    };
+    let output = [];
+    for(i=0;i < num; i++){
+        output.push(string);
+    }
+    return output.join('');
 };
 
 // Do not edit below this line
