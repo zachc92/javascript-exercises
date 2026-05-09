5e9267aa91059c155b59d005d37e6b94148809ac
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2026-05-09 13:50:01 -0400

    Complete calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((item, current) => item + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((item, current) => item * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a === 0){
+    return 1;
+  }
+	let number = a
+  for(i = a - 1; i > 0; i--){
+    number = number*i;
+  }
+  return number;
 };
 
 // Do not edit below this line
