baa7bb72dfc47754d3db8c75f1ab5d43c5015e75
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2026-05-09 15:22:20 -0400

    Complete find the oldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for(i=0; i<people.length;i++){
+        if(!people[i].yearOfDeath){
+            people[i].yearOfDeath = new Date().getFullYear();
+        }
+    }
+    let sorted = people.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
+    return sorted[0];
 };
 
 // Do not edit below this line
