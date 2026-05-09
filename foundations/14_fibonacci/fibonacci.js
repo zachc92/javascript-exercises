165a25dce7c0e9b71d2ed97b4ba434b72dbff424
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2026-05-09 15:01:56 -0400

    Complete fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num == 0){
+        return 0;
+    } else if (num < 0){
+        return "OOPS"
+    } else if (typeof(num) === "string") {
+        num = Number(num);
+    }
+    let first = 0;
+    let second = 1;
+    let current = 0;
+    for(i = 1; i < num; i++){
+        current = first + second;
+        first = second;
+        second = current;
+    }
+    return second;
 };
 
 // Do not edit below this line
